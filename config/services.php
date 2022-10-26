d3d1af4a85a583455aa2d3668336e8a05bfbb46a
cfantasia <123>
2022-10-26 12:52:16 +0900

    구글로그인 커밋(오탈자확인)

--- config/services.php
+++ config/services.php
@@ -37,6 +37,6 @@ return [
         'auth_provider_x509_cert_url'=>"https://www.googleapis.com/oauth2/v1/certs",
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
         'redirect'=>env('APP_URL')."/callback",
-        'javascript_origins' => env('APP_URL').
-    ]
+        'javascript_origins' => env('APP_URL')
+    ],
 ];
