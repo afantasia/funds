6a29d740447af07191314be5f27ff2def412d03a
cfantasia <123>
2022-11-01 16:54:30 +0900

    ymdhis cast 함수 처리

--- app/Models/NewsFeedModel.php
+++ app/Models/NewsFeedModel.php
@@ -5,12 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-
+use Carbon;
 class NewsFeedModel extends Model
 {
-    
+    protected $casts = [
+        'created_at' => "datetime:Y년 m월 d일 H시경",
+    ];
     use SoftDeletes;
     protected $table = 'news_feeds';
-    
     
 }
