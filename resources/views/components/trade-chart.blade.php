fda15736fd3fde06fe8df130b20b2bf25a4013d9
afantasia <innodb@kakao.com>
2025-01-13 14:46:08 +0900

    차트 관련 변경작업  - 추가 블레이드 템플릿 누락 추가

---
+++ resources/views/components/trade-chart.blade.php
@@ -0,0 +1,37 @@
+<div class="card">
+    <div class="card-header">
+        <div class="d-flex justify-content-between">
+            <div>
+                <h1>차트보기</h1>
+            </div>
+        </div>
+    </div>
+    <div class="card-body">
+        <canvas id="recentChartCtx"></canvas>
+
+    </div>
+</div>
+<script>
+    $(document).ready(function(){
+        // 랜덤 색상 생성 함수
+        function getRandomColor() {
+            const letters = '0123456789ABCDEF';
+            let color = '#';
+            for (let i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
+        }
+        axios.post("/stock/recentHistory",{}).then((result)=> {
+            console.log(result.data.datas);
+            var ctDatas=result.data.datas;
+            // Chart.js 초기화
+            new Chart(document.getElementById("recentChartCtx"), {
+                type: "line",
+                data: {
+                    datasets: ctDatas, // 변환 없이 바로 사용
+                },
+            });
+        });
+    })
+</script>
