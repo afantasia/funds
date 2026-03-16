b2b545439566be77df24e99a0a8596288257cc50
afantasia <vgbgbg@gmail.com>
2026-03-16 17:37:28 +0900

    fix: replace env() with config() for app name in layout template

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,7 +9,7 @@
         })();
     </script>
     <meta charset="utf-8">
-    <title>{{env('APP_NAME','홈페이지')}}</title>
+    <title>{{ config('app.name') }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Custom fonts for this template-->
 
