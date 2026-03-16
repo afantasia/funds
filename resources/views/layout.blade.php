ffe6b7bf008d35189dbc6018e800ef3a3f47121e
afantasia <vgbgbg@gmail.com>
2026-03-16 17:29:17 +0900

    fix: update page title in layout template

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,7 +9,7 @@
         })();
     </script>
     <meta charset="utf-8">
-    <title>HOMEPAGE</title>
+    <title>{{env('APP_NAME','홈페이지')}}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Custom fonts for this template-->
 
