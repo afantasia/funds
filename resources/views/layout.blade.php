61a44da49c732d9ebb8bc95e603a2fa640871158
afantasia <vgbgbg@gmail.com>
2026-03-16 17:21:46 +0900

    fix: update page title in layout template

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,7 +9,7 @@
         })();
     </script>
     <meta charset="utf-8">
-    <title>ㅔ?</title>
+    <title>HOMEPAGE</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Custom fonts for this template-->
 
